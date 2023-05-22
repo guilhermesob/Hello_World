16ed7bffc7ed30ab3a0d45ea05deca5073ef6fed
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-05-22 17:22:01 -0300

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,12 @@
+<!DOCTYPE hmtl>
+<html lang="en">
+    <head>
+        <mEta charset="UTF-8">
+            <title> hello world</title>
+            <script>
+                alert("hello world")
+            </script>
+    </head>
+    <body>
+    </body>
+</html>
